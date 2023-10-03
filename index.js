cba63e4dd9e3ee26ce0f82b324e306892813515f
Jason <jason5donoghue@gmail.com>
2023-10-03 22:18:28 +0100

    receipt added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const products_1 = require("./products");
 let productName = "beanie";
-let shipping;
+let shipping = 0;
 let taxPercent;
 let taxTotal;
 let total;
@@ -23,4 +23,20 @@ for (let i = 0; i < products_1.default.length; i++) {
         shipping = 5;
     }
 }
+if (shippingAddress.match("New York")) {
+    taxPercent = 0.1;
+}
+else {
+    taxPercent = 0.05;
+}
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+console.log(`
+  Product: ${product.name}
+  Address: ${shippingAddress}
+  Price: £${product.price}\
+  Tax: £${taxTotal.toFixed(2)}
+  Shipping: £${shipping.toFixed(2)}
+  Total: £${total.toFixed(2)}
+  `);
 // console.log(products[0].preOrder);
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import products from "./products";
 
 let productName: string = "beanie";
-let shipping: number;
+let shipping: number = 0;
 let taxPercent: number;
 let taxTotal: number;
 let total: number;
-let shippingAddress: string = "55 Toronto Terrace"
+let shippingAddress: string = "55 Toronto Terrace";
 
 const product = products.filter((product) => product.name === productName)[0];
 // console.log(product);
@@ -20,12 +20,31 @@ for (let i = 0; i < products.length; i++) {
 
 for (let i = 0; i < products.length; i++) {
   if (products[i].price > 25) {
-    shipping = 0
-    console.log(`We provide free shipping for ${products[i].name}!`)
-    }
-  else if (products[i].price < 25) {
-    shipping = 5
+    shipping = 0;
+    console.log(`We provide free shipping for ${products[i].name}!`);
+  } else if (products[i].price < 25) {
+    shipping = 5;
+  }
 }
+
+if (shippingAddress.match("New York")) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
 }
+
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+
+console.log(
+  `
+  Product: ${product.name}
+  Address: ${shippingAddress}
+  Price: £${product.price}
+  Tax: £${taxTotal.toFixed(2)}
+  Shipping: £${shipping.toFixed(2)}
+  Total: £${total.toFixed(2)}
+  `
+);
 
 // console.log(products[0].preOrder);
