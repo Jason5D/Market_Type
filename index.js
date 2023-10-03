0c7c98bf07adff43d0782519c52e8d4e36d19e69
Jason <jason5donoghue@gmail.com>
2023-10-03 21:35:26 +0100

    shipping info added

--- index.js
+++ index.js
@@ -2,5 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const products_1 = require("./products");
 let productName = "beanie";
+let shipping;
+let taxPercent;
+let taxTotal;
+let total;
+let shippingAddress = "55 Toronto Terrace";
 const product = products_1.default.filter((product) => product.name === productName)[0];
-console.log(product);
+// console.log(product);
+for (let i = 0; i < products_1.default.length; i++) {
+    if (products_1.default[i].preOrder === true) {
+        console.log(`${products_1.default[i].name} - We'll send you a message when it's on it's way`);
+    }
+}
+for (let i = 0; i < products_1.default.length; i++) {
+    if (products_1.default[i].price > 25) {
+        shipping = 0;
+        console.log(`We provide free shipping for ${products_1.default[i].name}!`);
+    }
+    else if (products_1.default[i].price < 25) {
+        shipping = 5;
+    }
+}
+// console.log(products[0].preOrder);
--- index.ts
+++ index.ts
@@ -1,6 +1,31 @@
 import products from "./products";
 
 let productName: string = "beanie";
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress: string = "55 Toronto Terrace"
 
 const product = products.filter((product) => product.name === productName)[0];
-console.log(product);
+// console.log(product);
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].preOrder === true) {
+    console.log(
+      `${products[i].name} - We'll send you a message when it's on it's way`
+    );
+  }
+}
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].price > 25) {
+    shipping = 0
+    console.log(`We provide free shipping for ${products[i].name}!`)
+    }
+  else if (products[i].price < 25) {
+    shipping = 5
+}
+}
+
+// console.log(products[0].preOrder);
